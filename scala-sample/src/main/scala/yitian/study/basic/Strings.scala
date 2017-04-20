1d6efa6ad295548ec25a72a9735a71a29c74d840
yitian <techstay@aliyun.com>
2017-04-21 02:11:47 +0800

    添加了字符串互操作的示例代码。

--- scala-sample/src/main/scala/yitian/study/basic/Strings.scala
+++ scala-sample/src/main/scala/yitian/study/basic/Strings.scala
@@ -6,6 +6,7 @@ package yitian.study.basic
 object Strings {
   def main(args: Array[String]): Unit = {
     operateStrings()
+    stringInterpolation()
   }
 
   /**
@@ -22,4 +23,29 @@ object Strings {
     println(string.toUpperCase())
     println(string.toLowerCase())
   }
+
+  /**
+    * 字符串互操作
+    */
+  def stringInterpolation(): Unit = {
+    val name = "yitian"
+    val age = 25
+
+    //内插字符串
+    println(s"Hello, $name")
+    println(s"Hello, ${name.toUpperCase()}")
+    //格式化字符串
+    println(f"Hello, I'm $name%s, I'm $age%-5d.")
+    //原始字符串
+    println(raw"Hey, \n\n\t")
+
+    //自定义互操作符
+    println(hello"yitian")
+  }
+
+  implicit class HelloStringHelper(val context: StringContext) extends AnyVal {
+    def hello(args: Any*): String = s"Hello, ${context.parts.mkString("")}"
+  }
+
 }
+
